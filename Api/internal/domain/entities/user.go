e3c3663a3147d5b498d83feacc00a7234aba5855
Ricardo A Leyva <25676407+tatoand97@users.noreply.github.com>
2025-09-01 21:08:41 -0500

    Add city and country to user registration

--- Api/internal/domain/entities/user.go
+++ Api/internal/domain/entities/user.go
@@ -8,5 +8,7 @@ type User struct {
 	LastName     string `gorm:"not null"`
 	Email        string `gorm:"uniqueIndex;not null"`
 	PasswordHash string `gorm:"not null"`
+	City         string
+	Country      string
 	CreatedAt    time.Time
 }
